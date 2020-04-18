b1ff975a422114b644d3555dddabfda73f43140c
Brice Thomas <brice.p.thomas@gmail.com>
2020-04-18 11:00:23 +0200

    Picking up Plant

--- player/Player.gd
+++ player/Player.gd
@@ -9,13 +9,16 @@ export (int) var gravity = 2000
 
 var velocity = Vector2.ZERO
 var jumping = false
-
 var input_movement = false
 
+var plant_detected = false
+var plant_holding = false
 ###
 
+
 func _ready():
-	pass
+	if not plant_holding:
+		$Pickup/Plant.hide()
 
 
 func _input(event):
@@ -23,12 +26,11 @@ func _input(event):
 	
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed('ui_select')
+	var jump = Input.is_action_just_pressed('ui_up')
 	
 	input_movement = right or left
 	
 	if jump and is_on_floor():
-		print("ok")
 		jumping = true
 		velocity.y = jump_speed
 	if right and not is_on_wall():
@@ -36,6 +38,16 @@ func _input(event):
 	if left and not is_on_wall():
 		velocity.x -= run_speed
 	
+#	if left:
+#		$Camera.offset_h = -1
+#	elif right:
+#		$Camera.offset_h = 1
+	
+	if Input.is_action_just_pressed("ui_select") and plant_detected:
+		plant_detected.queue_free()
+		plant_holding = true
+		$Pickup/Plant.show()
+		$AnimationPlayer.play("flower_picking")
 
 
 func _physics_process(delta):
@@ -49,3 +61,16 @@ func _physics_process(delta):
 		velocity.x = 0
 	
 	velocity = move_and_slide(velocity, Vector2.UP, true, 4, deg2rad(80))
+	
+
+func _on_Detection_body_entered(body):
+	if body.name == "Plant":
+		$AnimationPlayer.play("flower_detected")
+		plant_detected = body
+
+
+func _on_Detection_body_exited(body):
+	if body.name == "Plant":
+		if not plant_holding:
+			$AnimationPlayer.play_backwards("flower_detected")
+		plant_detected = null
